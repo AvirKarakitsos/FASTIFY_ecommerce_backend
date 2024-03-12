1b338eed78367084525007cfcbba4035129e410d
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-03-12 17:45:28 +0100

    feat: add orders in database

--- config/resolvers.js
+++ config/resolvers.js
@@ -1,199 +1,199 @@
 
 export const configResolvers = (database) => {
-    const db = database.mongo.db
+	const db = database.mongo.db
 
-    async function getAll (collectionName) {
-        try {
-            const collection = db.collection(collectionName);
-            const result = await collection.find().toArray();
-            
-            return result
-        } catch(err) {
-            console.error("Error message: "+err)
-        }
-    }
+	async function getAll (collectionName) {
+		try {
+				const collection = db.collection(collectionName);
+				const result = await collection.find().toArray();
+				
+				return result
+		} catch(err) {
+				console.error("Error message: "+err)
+		}
+	}
 
-    async function getOne(collectionName, param) {
-        try {
-            const collection = db.collection(collectionName);
-            const result = await collection.find().toArray();
-            let filterResult = null
-            
-            if(param?.id) filterResult = result.filter((item) => item._id.toString() === param.id);
-            else if(param?.email) filterResult = result.filter((item) => item.email === param.email);
+	async function getOne(collectionName, param) {
+		try {
+			const collection = db.collection(collectionName);
+			const result = await collection.find().toArray();
 
-            if(!filterResult) return null
-            else return filterResult[0]
-        } catch(err) {
-            console.error("Error message: "+err)
-        }
-    }
+			let filterResult = null
+			
+			if(param?.id) filterResult = result.filter((item) => item._id.toString() === param.id);
+			else if(param?.email) filterResult = result.filter((item) => item.email === param.email);
 
-    async function hasMany(collectionName, param) {
-        try {
-            const collection = db.collection(collectionName);
-            const result = await collection.find().toArray();
-            let filterResult = null
-            
-            switch(collectionName) {
-                case "orders": 
-                    filterResult = result.filter((item) => item.userId.toString() === param._id.toString());
-                    break;
-                case "orders_products":
-                    if(param.name) filterResult = result.filter((item) => item.productId.toString() === param._id.toString());
-                    else filterResult = result.filter((item) => item.orderId.toString() === param._id.toString());
-                    break;
-                default:
-                    filterResult = []
-            }
-    
-            return filterResult
-        } catch(err) {
-            console.error("Error message: "+err)
-        }
-    }
+			if(!filterResult) return null
+			else return filterResult[0]
+		} catch(err) {
+			console.error("Error message: "+err)
+		}
+	}
 
-    async function belongsTo(collectionName, param) {
-        try {
-            const collection = db.collection(collectionName);
-            const result = await collection.find().toArray();
-            let filterResult = null
-            
-            switch(collectionName) {
-                case "users": 
-                    filterResult = result.filter((item) => item._id.toString() === param.userId.toString());
-                    break;
-                case "orders":
-                    filterResult = result.filter((item) => item._id.toString() === param.orderId.toString());
-                    break;
-                case "products":
-                    filterResult = result.filter((item) => item._id.toString() === param.productId.toString());
-                    break;
-                default:
-                    filterResult = []
-            }
-    
-            return filterResult[0]
-        } catch(err) {
-            console.error("Error message: "+err)
-        }
-    }
+	async function hasMany(collectionName, param) {
+		try {
+			const collection = db.collection(collectionName);
+			const result = await collection.find().toArray();
+			let filterResult = null
+			
+			switch(collectionName) {
+				case "orders": 
+						filterResult = result.filter((item) => item.userId.toString() === param._id.toString());
+						break;
+				case "orders_products":
+						if(param.name) filterResult = result.filter((item) => item.productId.toString() === param._id.toString());
+						else filterResult = result.filter((item) => item.orderId.toString() === param._id.toString());
+						break;
+				default:
+						filterResult = []
+			}
 
-    const resolvers = {
-        Query: {
-            //Get All
-            users() {
-                return getAll("users")
-            },
-            products() {
-                return getAll("products")
-            },
-            orders() {
-                return getAll("orders")
-            },
-            ordersProducts() {
-                return getAll("orders_products")
-            },
-            //Get One
-            // user(_, args) {
-            //     return getOne("users", args)
-            // },
-            async user(_, args, context) {
-                try {
-                    const collection = db.collection("users");
-                    const result = await collection.find().toArray();
-                    let filterResult = null
-                    console.log(context.userId)
-                    if(context.userId) filterResult = result.filter((item) => item._id.toString() === context.userId);
-                   
-                    if(!filterResult) return null
-                    else return filterResult[0]
-                } catch(err) {
-                    console.error("Error message: "+err)
-                }
-            },
-            product(_, args) {
-                return getOne("products", args)
-            },
-            order(_, args) {
-                return getOne("orders", args)
-            },
-            orderProduct(_, args) {
-                return getOne("orders_products", args)
-            },
-        },
-        User: {
-            orders(parent) {
-                return hasMany("orders",parent)
-            }
-        },
-        Order: {
-            user(parent) {
-                return belongsTo("users",parent)
-            },
-            products(parent) {
-                return hasMany("orders_products",parent)
-            }
-        },
-        Product: {
-            orders(parent) {
-                return hasMany("orders_products",parent)
-            }
-        },
-        OrderProduct: {
-            order(parent) {
-                return belongsTo("orders",parent)
-            },
-            product(parent) {
-                return belongsTo("products",parent)
-            }
-        },
-        Mutation: {
-            async validOrder(_, args, context) {
-                try {
-                    const {reply, userId} = context
-        
-                    if(!userId) return reply.code(401).send({ message: 'Unauthorized' });
-                    
-                    const session = await database.stripe.checkout.sessions.create({
-                        payment_method_types: ['card'],
-                        mode: 'payment',
-                        line_items: args.order.map(product => {
-                            return {
-                                price_data: {
-                                    currency: 'usd',
-                                    product_data: {name: product.name},
-                                    unit_amount: product.unity
-                                },
-                                quantity: product.quantity
-                            }
-                        }),
-                        success_url: "http://localhost:5173/",
-                        cancel_url: "http://localhost:5173/",
-                    });
-                
-                    return {url: session.url};
+			return filterResult
+		} catch(err) {
+			console.error("Error message: "+err)
+		}
+	}
 
-                } catch (error) {
-                    console.error('Erreur lors de la création de la session Checkout:', error);
-                }
-            },
-            async loginUser(_,args) {
-                try {
-                    const user = await getOne("users", args.user)
-                    if(!user) {
-                        throw new Error("error 400: unauthorized")
-                    } 
-                    
-                    const token = database.jwt.sign({ userId: user._id })
-                    return { token }
-                } catch(err) {
-                    console.error("Error message: "+err)
-                }
-                
-            }
-        }
-    }
+	async function belongsTo(collectionName, param) {
+		try {
+			const collection = db.collection(collectionName);
+			const result = await collection.find().toArray();
+			let filterResult = null
+			
+			switch(collectionName) {
+					case "users": 
+							filterResult = result.filter((item) => item._id.toString() === param.userId.toString());
+							break;
+					case "orders":
+							filterResult = result.filter((item) => item._id.toString() === param.orderId.toString());
+							break;
+					case "products":
+							filterResult = result.filter((item) => item._id.toString() === param.productId.toString());
+							break;
+					default:
+							filterResult = []
+			}
 
-    return resolvers;
+			return filterResult[0]
+		} catch(err) {
+				console.error("Error message: "+err)
+		}
+	}
+
+	const resolvers = {
+		Query: {
+				//Get All
+			users() {
+				return getAll("users")
+			},
+			products() {
+				return getAll("products")
+			},
+			orders() {
+				return getAll("orders")
+			},
+			ordersProducts() {
+				return getAll("orders_products")
+			},
+			//Get One
+			async user(_, args, context) {
+				try {
+					const collection = db.collection("users");
+					const result = await collection.find().toArray();
+					let filterResult = null
+					console.log(context.userId)
+					if(context.userId) filterResult = result.filter((item) => item._id.toString() === context.userId);
+					
+					if(!filterResult) return null
+					else return filterResult[0]
+				} catch(err) {
+						console.error("Error message: "+err)
+				}
+			},
+			product(_, args) {
+				return getOne("products", args)
+			},
+			order(_, args) {
+				return getOne("orders", args)
+			},
+			orderProduct(_, args) {
+				return getOne("orders_products", args)
+			},
+		},
+		User: {
+			orders(parent) {
+				return hasMany("orders",parent)
+			}
+		},
+		Order: {
+			user(parent) {
+				return belongsTo("users",parent)
+			},
+			products(parent) {
+				return hasMany("orders_products",parent)
+			}
+		},
+		Product: {
+			orders(parent) {
+				return hasMany("orders_products",parent)
+			}
+		},
+		OrderProduct: {
+			order(parent) {
+				return belongsTo("orders",parent)
+			},
+			product(parent) {
+				return belongsTo("products",parent)
+			}
+		},
+		Mutation: {
+			async validOrder(_, args, context) {
+				try {
+					const {reply, userId} = context
+
+					if(!userId) return reply.code(401).send({ message: 'Unauthorized' });
+					
+					const session = await database.stripe.checkout.sessions.create({
+						payment_method_types: ['card'],
+						mode: 'payment',
+						line_items: args.order.map(product => {
+							return {
+								price_data: {
+									currency: 'usd',
+									product_data: {
+											name: product.name
+									},
+									unit_amount: product.unity
+								},
+								quantity: product.quantity,
+							}
+						}),
+							success_url: "http://localhost:5173/",
+							cancel_url: "http://localhost:5173/"
+						});
+			
+					return {url: session.url};
+
+				} catch (error) {
+						console.error('Erreur lors de la création de la session Checkout:', error);
+				}
+			},
+			async loginUser(_,args) {
+				try {
+					const user = await getOne("users", args.user)
+					if(!user) {
+						throw new Error("error 400: unauthorized")
+					} 
+					
+					const token = database.jwt.sign({ userId: user._id })
+					return { token }
+				} catch(err) {
+					console.error("Error message: "+err)
+				}
+					
+			}
+		}
+	}
+
+	return resolvers;
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import fastify from 'fastify';
 import fastifyEnv from '@fastify/env';
-import fastifyMongoDB from '@fastify/mongodb';
+import fastifyMongoDB, { ObjectId } from '@fastify/mongodb';
 import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
 import fastifyStripe from "fastify-stripe"
@@ -9,81 +9,161 @@ import { schema } from './config/schema.js';
 import { configResolvers } from './config/resolvers.js';
 
 const app = fastify({
-  logger: true
+	logger: true
 });
 
 app.register(fastifyCors, {
-  origin: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
+	origin: true,
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	credentials: true,
 });
 
 app.register(fastifyEnv, {
-  dotenv: true,
-  schema: {
-    type: 'object',
-    required: ['DB_CONNECTION','SECRET_KEY','JWT_KEY'],
-    properties: {
-        DB_CONNECTION: {
-        type: 'string',
-      },
-      SECRET_KEY: {
-        type: 'string',
-      },
-      JWT_KEY: {
-        type: 'string',
-      },
-    }
-  }
+	dotenv: true,
+	schema: {
+		type: 'object',
+		required: ['DB_CONNECTION','SECRET_KEY','JWT_KEY'],
+		properties: {
+				DB_CONNECTION: {
+				type: 'string',
+			},
+			SECRET_KEY: {
+				type: 'string',
+			},
+			JWT_KEY: {
+				type: 'string',
+			},
+		}
+	}
 })
 
 await app
 
 app.register(fastifyMongoDB, {
-  url: app.config.DB_CONNECTION,
-  database: "shop"
+	url: app.config.DB_CONNECTION,
+	database: "shop"
 });
 
 await app
 
 app.register(fastifyJwt, {
-  secret: app.config.JWT_KEY,
-  sign: {
-    expiresIn: '1h'
-  }
+	secret: app.config.JWT_KEY,
+	sign: {
+		expiresIn: '1h'
+	}
 })
 
 await app
 
 app.register(fastifyStripe, {
-  apiKey: app.config.SECRET_KEY
+	apiKey: app.config.SECRET_KEY
 })
  
 await app
 
 app.register(mercurius,{
-  schema,
-  resolvers: configResolvers(app),
-  context: (request, reply) => {
-    const token = request.headers.authorization?.split(" ")[1]
-    if(!token) return {userId: null, reply}
-    else {
-      const decodeToken = app.jwt.verify(token)
+	schema,
+	resolvers: configResolvers(app),
+	context: (request, reply) => {
+		const token = request.headers.authorization?.split(" ")[1]
+		if(!token) return {userId: null, reply}
+		else {
+			const decodeToken = app.jwt.verify(token)
 
-      return { userId: decodeToken.userId, reply }
-    }
-  }
+			return { userId: decodeToken.userId, reply }
+		}
+	}
 })
-  
+
+await app
+
+app.post('/webhook', (request, reply) => {
+	const event = request.body;
+	
+	switch (event.type) {
+		case 'payment_intent.succeeded':
+			console.log("Here")
+			console.log(event)
+			const paymentIntent = event;
+			console.log(paymentIntent.data.object)
+			// Then define and call a method to handle the successful payment intent.
+			// handlePaymentIntentSucceeded(paymentIntent);
+			break;
+		case 'payment_intent.canceled':
+			const paymentIntentCanceled = event.data.object;
+			// Then define and call a function to handle the event payment_intent.canceled
+			break;
+		case 'payment_method.attached':
+			const paymentMethod = event.data.object;
+			// Then define and call a method to handle the successful attachment of a PaymentMethod.
+			// handlePaymentMethodAttached(paymentMethod);
+			break;
+		// ... handle other event types
+		default:
+			console.log(`Unhandled event type ${event.type}`);
+	}
+
+	// Return a response to acknowledge receipt of the event
+	reply.send({event: "succed"})
+})
+
+app.get("/res", async (req, rep) => {
+	try {
+		const payments_in_progress = {
+			sessionId: "cs_test_b1qMFfYE4lO4SRnYJJ66iOepZHcaSsWyLm6ooNwzne1ZZ1w2AL0iuLYGbZ",
+			userId: "65ddc19ab2a2c36b28f5992e"
+		}
+		
+		const session = await app.stripe.checkout.sessions.retrieve(
+			payments_in_progress.sessionId,
+			{
+				expand: ['line_items'],
+			}
+		);
+
+		const addOrder = {
+			userId: new ObjectId(payments_in_progress.userId),
+			totalPrice: session.amount_total,
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}
+
+		const ordersCollection = app.mongo.db.collection("orders")
+		
+		const newOrder = await ordersCollection.insertOne(addOrder)
+		
+		
+		const order_productCollection = app.mongo.db.collection("orders_products")
+		const allProducts = await app.mongo.db.collection("products").find().toArray()
+		
+		const newProducts = session.line_items.data.map((newProduct) => {
+			const filterProduct = allProducts.filter(product => product.name === newProduct.description)
+	
+			return {
+				orderId: newOrder.insertedId,
+				productId: filterProduct[0]._id,
+				quantity: newProduct.quantity
+			}
+		})
+		
+		await order_productCollection.insertMany(newProducts)
+	
+	} catch(err) {
+		console.error(err);
+	}
+	rep.send({event: "finished"})
+})
+
+	
 // Démarre le serveur Fastify
 const start = async () => {
-  try {
-    await app.listen({port: 3000});
-    console.log('Serveur Fastify en cours d\'exécution sur le port 3000');
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+	try {
+		await app.listen({port: 3000});
+		console.log('Serveur Fastify en cours d\'exécution sur le port 3000');
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 };
 
 start();
